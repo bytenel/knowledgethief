e701b711adfbff406a1822e02e7bd19116e2ab98
Landon Wilkins <landonwilkins@gmail.com>
2013-04-15 23:56:02 -0600

    error catching

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   #include PublicActivity::StoreController
-
+  rescue_from(Exception) { # Or just handle particular exceptions
+    # do stuff
+    redirect_to home_path
+  }
 
   include Engage::Extensions::Helpers
     protect_from_forgery
